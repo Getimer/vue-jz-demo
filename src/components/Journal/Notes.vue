14a9adfed02a01f1ec9af144ea2dc04b960a5db2
nmer <2309382194@qq.com>
2023-04-03 17:28:04 +0800

    模块化:拆分è®Journal成三个子组件

---
+++ src/components/Journal/Notes.vue
@@ -0,0 +1,35 @@
+<template>
+    <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里添加备注">
+    </label>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Notes'
+    };
+</script>
+
+<style lang="scss" scoped>
+    .notes {
+        display: flex;
+        background: #f5f5f5;
+        font-size: 14px;
+        padding-left: 16px;
+        align-items: center;
+
+        > .name {
+            padding-right: 16px;
+        }
+
+        input {
+
+            margin-left: 2px;
+            height: 64px;
+            flex-grow: 1;
+            background: transparent;
+            border: none;
+        }
+    }
+</style>
---
+++ src/components/Journal/Tags.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="tags">
+        <div class="add">
+            <button>新增标签</button>
+        </div>
+        <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ul>
+
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Tags'
+    };
+</script>
+
+<style lang="scss" scoped>
+    .tags {
+        display: flex;
+        font-size: 14px;
+        padding: 16px;
+        flex-grow: 1;
+        flex-direction: column-reverse;
+
+        > .current {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+
+            > li {
+                background: #d9d9d9;
+                $h: 24px;
+                height: $h;
+                border-radius: $h/2;
+                padding: 0 16px;
+                margin-right: 12px;
+                margin-top: 5px;
+            }
+        }
+
+        > .add {
+            margin-top: 16px;
+
+            button {
+                background: transparent;
+                border: none;
+                border-bottom: 1px solid;
+                color: #999999;
+                padding-right: 2px;
+                padding-left: 2px;
+            }
+        }
+    }
+</style>
