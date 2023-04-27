5a7f89a1198e3d5b0ac353c3d8eb509e9694464a
nmer <2309382194@qq.com>
2023-04-27 11:39:08 +0800

    添加二维码 保证用户用手机打开

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,18 @@ import Layout from '@/components/Layout.vue';
 createApp(App).component('Nav', Nav).component('Layout', Layout).component('Icon', Icon).use(store).use(router).mount('#app');
 window.onload=function(){
     setTimeout(function() {
-        window.scrollTo(0, 99999)
+        window.scrollTo(0, 200)
     }, 0);
 };
+
+if(window.document.body.clientWidth>500){
+    window.alert('请使用手机打开该地址以保证预览效果')
+    const img=document.createElement('img')
+    img.src='./qrcode.png'
+    img.style.position='fixed'
+    img.style.left='50%'
+    img.style.top='50%'
+    img.style.transform='translate(-50%,-50%)'
+    img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)'
+    document.body.appendChild(img)
+}
